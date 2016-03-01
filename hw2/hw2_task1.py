138af9aa0a28d60a8b9b9f0d80e594cddeb2b931
Weifan Lin <wlin15@citymail.cuny.edu>
2016-02-29 23:30:35 -0500

    get rid of using pandas read_csv

--- hw2/hw2_task1.py
+++ hw2/hw2_task1.py
@@ -7,9 +7,7 @@ Homework 2, task 1
 
 import csv
 import sys
-import pandas as pd
 from datetime import date
-import numpy as np
 import collections
 
 if __name__=='__main__':
@@ -17,15 +15,18 @@ if __name__=='__main__':
 		sys.stderr.write('USAGE: python %s <INPUT_CSV>\n' % sys.argv[0])
 		sys.exit(1)
 
-	citibike = pd.read_csv(sys.argv[1])
-	steam = list(citibike['birth_year']).__iter__()
+
 	total = 0
 	count = {}
 
-	for i in steam:
-		if not np.isnan(i): # an empty b-year can be read as nan in type of 'numpy.float64'
-			count[i] = count.get(i, 0) + 1
-			total += 1
+	with open(sys.argv[1], 'r') as fi:
+		reader = csv.DictReader(fi)
+		for row in reader:
+			if row['usertype'] == 'Subscriber':  # we only want age of subscribers
+				b_year = row['birth_year']
+				count[b_year] = count.get(b_year, 0) + 1
+				total += 1
+
 
 	d = collections.OrderedDict(sorted(count.iteritems()))
 	'''
@@ -35,13 +36,13 @@ if __name__=='__main__':
 	'''
 
 	# using the same method to find median as in class
+	median_age = 0
 	agg = 0
 	for k, v in d.iteritems():
 		agg += v
 		if agg * 2 > total:
-			median = k 	# median here is b-year and its in type of 'numpy.float64', we want age.
+			median = k 	# median found but it is b-year, we want age.
+			median_age = date.today().year - int(median) # age = today's year - b-year
 			break	# once median is found break the for loop
 
-	# age = today's year - b-year
-	median_age = date.today().year - int(median)
 	print median_age
